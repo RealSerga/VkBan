691599a8a2cbbad55baac06a0c2772434d05fde4
RealSerga <RealSerga@DESKTOP-619UUPR>
2016-12-18 22:37:37 +0500

    Добавьте файлы проекта.

---
+++ VkBan/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace VkBan
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.PassTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.KeyTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.CaptchButton = new System.Windows.Forms.Button();
+            this.CatchTextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.miniprogressBar = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OkButton.Location = new System.Drawing.Point(12, 130);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(156, 47);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginTextBox.Location = new System.Drawing.Point(12, 30);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(158, 26);
+            this.LoginTextBox.TabIndex = 1;
+            // 
+            // PassTextBox
+            // 
+            this.PassTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PassTextBox.Location = new System.Drawing.Point(12, 87);
+            this.PassTextBox.Name = "PassTextBox";
+            this.PassTextBox.Size = new System.Drawing.Size(158, 26);
+            this.PassTextBox.TabIndex = 4;
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.KeyTextBox.Location = new System.Drawing.Point(196, 155);
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.Size = new System.Drawing.Size(365, 22);
+            this.KeyTextBox.TabIndex = 5;
+            this.KeyTextBox.Text = "вейп,cs go,раздача,ключи";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Email, Login, Mobile phone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(230, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ниже пишем ключевые слова через ,";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(207, 9);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(233, 104);
+            this.webBrowser.TabIndex = 9;
+            this.webBrowser.Visible = false;
+            // 
+            // CaptchButton
+            // 
+            this.CaptchButton.Location = new System.Drawing.Point(469, 69);
+            this.CaptchButton.Name = "CaptchButton";
+            this.CaptchButton.Size = new System.Drawing.Size(75, 23);
+            this.CaptchButton.TabIndex = 10;
+            this.CaptchButton.Text = "ОК Капча";
+            this.CaptchButton.UseVisualStyleBackColor = true;
+            this.CaptchButton.Visible = false;
+            this.CaptchButton.Click += new System.EventHandler(this.CaptchButton_Click);
+            // 
+            // CatchTextBox
+            // 
+            this.CatchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CatchTextBox.Location = new System.Drawing.Point(469, 30);
+            this.CatchTextBox.Name = "CatchTextBox";
+            this.CatchTextBox.Size = new System.Drawing.Size(75, 26);
+            this.CatchTextBox.TabIndex = 11;
+            this.CatchTextBox.Visible = false;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 199);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(549, 23);
+            this.progressBar.TabIndex = 12;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // miniprogressBar
+            // 
+            this.miniprogressBar.Location = new System.Drawing.Point(12, 183);
+            this.miniprogressBar.Name = "miniprogressBar";
+            this.miniprogressBar.Size = new System.Drawing.Size(549, 10);
+            this.miniprogressBar.TabIndex = 13;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 226);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(575, 22);
+            this.statusStrip1.TabIndex = 14;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 248);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.miniprogressBar);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.CatchTextBox);
+            this.Controls.Add(this.CaptchButton);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KeyTextBox);
+            this.Controls.Add(this.PassTextBox);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.OkButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "VkBan";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.MaskedTextBox PassTextBox;
+        private System.Windows.Forms.TextBox KeyTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button CaptchButton;
+        private System.Windows.Forms.TextBox CatchTextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ProgressBar miniprogressBar;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ VkBan/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Exception;
+using VkNet.Model.RequestParams;
+
+namespace VkBan
+{
+    public partial class MainForm : Form
+    {
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+        VkApi vk = new VkApi();
+        long captchssid;
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            
+
+
+            try
+            {
+                
+                vk.Authorize(new ApiAuthParams
+                {
+                    ApplicationId = 5783401,
+                    Login = LoginTextBox.Text,
+                    Password = PassTextBox.Text,
+                    Settings = VkNet.Enums.Filters.Settings.All
+                });
+               
+                
+                MessageBox.Show("Авторизация успешна","Ok",MessageBoxButtons.OK,MessageBoxIcon.Question);
+                toolStripStatusLabel.Text = "Авторизация успешна";
+                KeyTextBox.ReadOnly = true;
+                backgroundWorker.RunWorkerAsync();
+            }catch (CaptchaNeededException exe)
+            {
+                MessageBox.Show("Нужна капча", "Капча", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                toolStripStatusLabel.Text = "Капча";
+                webBrowser.Visible = true;
+                CatchTextBox.Visible = true;
+                CaptchButton.Visible = true;
+                webBrowser.Url = exe.Img;
+                captchssid = exe.Sid;
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+            
+        }
+
+        private void CaptchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                toolStripStatusLabel.Text = "Авторизуйтесь";
+                vk.Authorize(new ApiAuthParams
+                {
+                    ApplicationId = 5783401,
+                    Login = LoginTextBox.Text,
+                    Password = PassTextBox.Text,
+                    CaptchaKey = CatchTextBox.Text,
+                    CaptchaSid = captchssid
+                });
+            }catch (CaptchaNeededException exe)
+            {
+                MessageBox.Show("Нужна капча", "Капча", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                toolStripStatusLabel.Text = "Капча";
+                webBrowser.Visible = true;
+                CatchTextBox.Visible = true;
+                CaptchButton.Visible = true;
+                webBrowser.Url = exe.Img;
+                captchssid = exe.Sid;
+
+            }
+    webBrowser.Visible = false;
+            CatchTextBox.Visible = false;
+            CaptchButton.Visible = false;
+            OkButton_Click(sender, e);
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            String[] keys = KeyTextBox.Text.Split(',');
+            int j = 0;
+            foreach (var key in keys)
+            {
+                j++;
+                for (int i = 1; i < 3; i++)
+                {
+                    int totalCount;
+                    var groups = vk.Groups.Search(out totalCount, new GroupsSearchParams { Query = key, Offset = 100 * i - 100, Count = 100 * i });
+                    List<long> banslist = new List<long>();
+                    int d = 0;
+                    foreach (var gro in groups)
+                    {
+                        d++;
+                        banslist.Add(gro.Id);
+                        File.AppendAllText("ids.txt", gro.Id + ";");
+                        backgroundWorker.ReportProgress((100/groups.Count) * d);
+
+                    }
+
+                    try
+                    {
+                        vk.NewsFeed.AddBan(new List<long>(), banslist);
+                    }
+                    catch {
+                        
+                    }
+                    banslist.Clear();
+                }
+
+                backgroundWorker.ReportProgress(((100 / keys.Length) * j) * 10000);
+
+                
+
+
+
+
+            }
+            MessageBox.Show("Успешно!", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage < 10000)
+            {
+                if (e.ProgressPercentage <= 100 && 0 <= e.ProgressPercentage)
+
+                {
+                    miniprogressBar.Value = e.ProgressPercentage;
+                    if (progressBar.Value<1)
+                    {
+                        progressBar.Value = 1;
+                    }
+                }
+            }
+            else
+            {
+                if (e.ProgressPercentage / 10000 <= 100 && 0 <= e.ProgressPercentage / 10000)
+
+                {
+                    progressBar.Value = (e.ProgressPercentage / 10000);
+                }
+                
+            }
+            toolStripStatusLabel.Text = "Отправка";
+        }
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
